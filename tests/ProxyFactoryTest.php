bb8880761eb59ada10bbb4ae2bc799e1545d66d2
Jérôme Velociter <jerome@velociter.fr>
2015-08-10 20:25:46 +0200

    Slightly improve test

--- tests/ProxyFactoryTest.php
+++ tests/ProxyFactoryTest.php
@@ -2,12 +2,13 @@
 
 class ProxyFactoryTests extends \Proxy_TestCase {
 
-    public function test() {
+    public function testSimpleProxyFactory() {
         $this->withCassette("5185415ba171ea3a00704eed");
         $this->start();
         $response = $this->get("5185415ba171ea3a00704eed");
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals("{\"hello\": \"world\"}", $response->getBody()->getContents());
+        $this->assertEquals("application/json; charset=utf-8", $response->getHeaderLine("Content-Type"));
     }
 
 }
